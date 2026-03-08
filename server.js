08d68c259d457db708b4d539aa2c125a72b0305f
Tomek Jakub <tomekjakubiec35@gmail.com>
2026-03-08 19:18:44 +0100

    initial api

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+
+const excuses = require("./excuses.json");
+
+const PORT = process.env.PORT || 3000;
+
+function randomExcuse(category) {
+    const list = excuses[category];
+    return list[Math.floor(Math.random() * list.length)];
+}
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Excuse Generator API",
+        endpoints: [
+            "/excuse/school",
+            "/excuse/dev",
+            "/excuse/life",
+            "/excuse/random"
+        ]
+    });
+});
+
+app.get("/excuse/random", (req, res) => {
+    const categories = Object.keys(excuses);
+    const randomCategory = categories[Math.floor(Math.random() * categories.length)];
+
+    res.json({
+        category: randomCategory,
+        excuse: randomExcuse(randomCategory)
+    });
+});
+
+app.get("/excuse/:category", (req, res) => {
+    const category = req.params.category.toLowerCase();
+
+    if (!excuses[category]) {
+        return res.status(404).json({
+            error: "Category not found"
+        });
+    }
+
+    res.json({
+        category: category,
+        excuse: randomExcuse(category)
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
